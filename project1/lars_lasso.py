ce1c8ed226b8e04d1875de148d18485ebda34edf
Daniel Maag <danielmaag@gmail.com>
2017-04-03 16:48:37 +0200

    some stuff done

--- project1/lars_lasso.py
+++ project1/lars_lasso.py
@@ -37,7 +37,7 @@ def main():
     #Lasso(alpha=1.0, fit_intercept=True, normalize=False, precompute=False, copy_X=True, max_iter=1000, tol=0.0001, warm_start=False, positive=False, random_state=None, selection='cyclic')
     #LassoLars(alpha=1.0, fit_intercept=True, verbose=False, normalize=True, precompute='auto', max_iter=500, eps=2.2204460492503131e-16, copy_X=True, fit_path=True, positive=False)
 
-    param_grid = [{'alpha':np.logspace(-4, 10, 1000)}]
+    param_grid = [{'alpha':np.logspace(-10, 10, 200)}]
     model = GridSearchCV(linear_model.LassoLars(), param_grid, cv=5, scoring=None, fit_params=None, n_jobs=-1, iid=True, refit=True, verbose=1, pre_dispatch='2*n_jobs', error_score='raise', return_train_score=True)
 
     print("Training...")
