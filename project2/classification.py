5e33293e5b0a4e98a72df534a41b4b7f4902f330
Daniel <daniel.fischmann0@gmail.com>
2017-04-10 16:59:52 +0200

    Medium baseline project 2

--- project2/classification.py
+++ project2/classification.py
@@ -26,10 +26,7 @@ def main():
     # split for validation testing
     x_train, x_validate, y_train, y_validate = train_test_split(X, Y)
 
-    model = RandomForestClassifier(n_estimators=10, criterion='gini', max_depth=None, min_samples_split=2, 
-        min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, 
-        min_impurity_split=1e-07, bootstrap=True, oob_score=False, n_jobs=-1, random_state=None, 
-        verbose=0, warm_start=False, class_weight=None)
+    model = RandomForestClassifier()
 
 
     print("Training...")
