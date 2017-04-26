3061aa6456bd79704643f7828a41576a79f884c0
Fabian Schneider <schneider_fabian@bluewin.ch>
2017-04-26 19:50:23 +0200

    First working example of DNNClassifier
    got 0.9ish submission

--- project3/dnn.py
+++ project3/dnn.py
@@ -1,30 +1,49 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from sklearn.metrics import accuracy_score
 
 def main():
     train = pd.read_hdf("train.h5", "train")
     test = pd.read_hdf("test.h5", "test")
 
+    Y=train.iloc[0:,0]
+    X=train.iloc[0:,1:]
+    X_t=test.iloc[0:,0:]
+    t_id=test.index.values
+
+    Y=np.array(Y.values).astype('int')
+    X=np.array(X.values).astype('double')
+    X_t=np.array(X_t.values).astype('double')
+
+    # Train
+    feature_columns = [tf.contrib.layers.real_valued_column("", dimension=100)]
+    classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
+                                              hidden_units=[10, 20],
+                                              n_classes=5,
+                                              model_dir="/tmp/model")
+    # Define the training inputs
+    def get_train_inputs():
+        x = tf.constant(X)
+        y = tf.constant(Y)
+        return x, y
+
+    # Train a.k.a. Fit
+    classifier.fit(input_fn=get_train_inputs, steps=1000)
+
+    # accuracy_score = classifier.evaluate(input_fn=get_train_inputs)["accuracy"]
+    # print('Accuracy: {0:f}'.format(accuracy_score))
+
+    # Predict
+    predictions = list(classifier.predict(X_t))
 
 
-    # print mean squared error as first estimate
-    acc = accuracy_score(y_validate, model.predict(x_validate))
-    print("Categorisation accuracy:")
-    print(acc)
+    results_df = pd.DataFrame(data={'y':predictions})
+    joined = pd.DataFrame({'Id':t_id}).join(results_df)
 
-    print("Predicting...")
-    # assemble data for csv creation
-    y_prediction = model.predict(x_test)
-    results_df = pd.DataFrame(data={'y':y_prediction})
-    joined = pd.DataFrame(t_id).join(results_df)
-
-    # save data in csv format for submission
+    # Save data in csv format for submission
     print("Writing predictions to predictions.csv")
     joined.to_csv("predictions.csv", index=False)
-
-    acc = accuracy_score(y, y_pred)
-
 
 if __name__ == '__main__':
     main()
