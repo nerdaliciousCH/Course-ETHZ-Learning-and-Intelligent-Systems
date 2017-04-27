7065bd6b20c30353532872e1c367923a53491412
Fabian Schneider <schneider_fabian@bluewin.ch>
2017-04-27 15:18:48 +0200

    Medium Baseline

--- project3/dnn.py
+++ project3/dnn.py
@@ -1,41 +1,64 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import logging
 from sklearn.metrics import accuracy_score
+import sklearn.model_selection as sk
+
 
 def main():
     train = pd.read_hdf("train.h5", "train")
-    test = pd.read_hdf("test.h5", "test")
+    predict = pd.read_hdf("test.h5", "test")
 
     Y=train.iloc[0:,0]
     X=train.iloc[0:,1:]
-    X_t=test.iloc[0:,0:]
-    t_id=test.index.values
+    X_predict=predict.iloc[0:,0:]
+    t_id=predict.index.values
 
     Y=np.array(Y.values).astype('int')
     X=np.array(X.values).astype('double')
-    X_t=np.array(X_t.values).astype('double')
+    X_predict=np.array(X_predict.values).astype('double')
+
+    X_train, X_test, Y_train, Y_test = sk.train_test_split(X,Y,test_size=0.05, random_state=42)
+
+    #logging.getLogger().setLevel(logging.INFO)
+    #tf.logging.set_verbosity(tf.logging.INFO)
+    tf.logging.set_verbosity(tf.logging.ERROR)
 
     # Train
     feature_columns = [tf.contrib.layers.real_valued_column("", dimension=100)]
+
+    # tanh and softsign are the best activation functions so far
     classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
-                                              hidden_units=[10, 20],
-                                              n_classes=5,
-                                              model_dir="/tmp/model")
+                                              hidden_units=[128,256,128],
+                                                optimizer=tf.train.AdamOptimizer(learning_rate=0.001),
+                                                enable_centered_bias=True,
+                                                activation_fn=tf.nn.tanh,
+                                                n_classes=5)
     # Define the training inputs
     def get_train_inputs():
-        x = tf.constant(X)
-        y = tf.constant(Y)
+        x = tf.constant(X_train)
+        y = tf.constant(Y_train)
+        return x, y
+    
+    def get_test_inputs():
+        x = tf.constant(X_test)
+        y = tf.constant(Y_test)
         return x, y
 
     # Train a.k.a. Fit
+    print("Training...")
     classifier.fit(input_fn=get_train_inputs, steps=1000)
 
-    # accuracy_score = classifier.evaluate(input_fn=get_train_inputs)["accuracy"]
-    # print('Accuracy: {0:f}'.format(accuracy_score))
+    ## following line takes forever or doesn't halt... I don't know why
+    # accuracy_score = classifier.evaluate(input_fn=get_test_inputs)["accuracy"]
+    
+    accuracy = accuracy_score(Y_test, list(classifier.predict(X_test)))
+    print('Accuracy: {0:f}'.format(accuracy))
 
     # Predict
-    predictions = list(classifier.predict(X_t))
+    print("Making predictions...")
+    predictions = list(classifier.predict(X_predict))
 
 
     results_df = pd.DataFrame(data={'y':predictions})
