cc461003fd6f9f30a0bfaa2daaccafb45c3adae1
Fabian Schneider <schneider_fabian@bluewin.ch>
2017-05-09 18:57:53 +0200

    Beautiful custom DNN

--- project3/dnn.py
+++ project3/dnn.py
@@ -1,75 +1,77 @@
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-import logging
-from sklearn.metrics import accuracy_score
-import sklearn.model_selection as sk
-
-
-def main():
-
-    # read input data from hd5 format
-    train = pd.read_hdf("train.h5", "train")
-    predict = pd.read_hdf("test.h5", "test")
-
-    # create matrices from input
-    Y = train.iloc[0:,0]
-    X = train.iloc[0:,1:]
-    X_predict = predict.iloc[0:,0:]
-    t_id = predict.index.values
-
-    # type conversion
-    Y = np.array(Y.values).astype('int')
-    X = np.array(X.values).astype('double')
-    X_predict = np.array(X_predict.values).astype('double')
-
-    X_train, X_test, Y_train, Y_test = sk.train_test_split(X,Y,test_size=0.05, random_state=42)
-
-    #logging.getLogger().setLevel(logging.INFO)
-    #tf.logging.set_verbosity(tf.logging.INFO)
-    tf.logging.set_verbosity(tf.logging.ERROR)
-
-    # train
-    feature_columns = [tf.contrib.layers.real_valued_column("", dimension=100)]
-
-    # tanh and softsign are the best activation functions so far
-    classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
-                                              hidden_units=[128,256,128],
-                                                optimizer=tf.train.AdamOptimizer(learning_rate=0.001),
-                                                enable_centered_bias=True,
-                                                activation_fn=tf.nn.tanh,
-                                                n_classes=5)
-    # Define the training inputs
-    def get_train_inputs():
-        x = tf.constant(X_train)
-        y = tf.constant(Y_train)
-        return x, y
-    
-    def get_test_inputs():
-        x = tf.constant(X_test)
-        y = tf.constant(Y_test)
-        return x, y
-
-    # Train a.k.a. Fit
-    print("Training...")
-    classifier.fit(input_fn=get_train_inputs, steps=1000)
-
-    ## following line takes forever or doesn't halt... I don't know why
-    # accuracy_score = classifier.evaluate(input_fn=get_test_inputs)["accuracy"]
-    
-    accuracy = accuracy_score(Y_test, list(classifier.predict(X_test)))
-    print('Accuracy: {0:f}'.format(accuracy))
-
-    # make the prediction
-    print("Making predictions...")
-    predictions = list(classifier.predict(X_predict))
-
-    results_df = pd.DataFrame(data={'y':predictions})
-    joined = pd.DataFrame({'Id':t_id}).join(results_df)
-
-    # Save data in csv format for submission
-    print("Writing predictions to predictions.csv")
-    joined.to_csv("predictions.csv", index=False)
-
-if __name__ == '__main__':
-    main()
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import logging
+from sklearn.metrics import accuracy_score
+import sklearn.model_selection as sk
+
+
+def main():
+
+    # read input data from hd5 format
+    train = pd.read_hdf("train.h5", "train")
+    predict = pd.read_hdf("test.h5", "test")
+
+    # create matrices from input
+    Y = train.iloc[0:,0]
+    X = train.iloc[0:,1:]
+    X_predict = predict.iloc[0:,0:]
+    t_id = predict.index.values
+
+    # type conversion
+    Y = np.array(Y.values).astype('int')
+    X = np.array(X.values).astype('double')
+    X_predict = np.array(X_predict.values).astype('double')
+
+    X_train, X_test, Y_train, Y_test = sk.train_test_split(X,Y,test_size=0.05, random_state=42)
+
+    #logging.getLogger().setLevel(logging.INFO)
+    #tf.logging.set_verbosity(tf.logging.INFO)
+    tf.logging.set_verbosity(tf.logging.ERROR)
+
+
+    # train
+    feature_columns = [tf.contrib.layers.real_valued_column("", dimension=100)]
+
+    # Train
+    # tanh and softsign are the best activation functions so far
+    classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
+                                              hidden_units=[256,512,256,128],
+                                                optimizer=tf.train.AdamOptimizer(learning_rate=0.001),
+                                                enable_centered_bias=True,
+                                                activation_fn=tf.nn.tanh,
+                                                n_classes=5)
+    # Define the training inputs
+    def get_train_inputs():
+        x = tf.constant(X_train)
+        y = tf.constant(Y_train)
+        return x, y
+
+    def get_test_inputs():
+        x = tf.constant(X_test)
+        y = tf.constant(Y_test)
+        return x, y
+
+    # Train a.k.a. Fit
+    print("Training...")
+    classifier.fit(input_fn=get_train_inputs, steps=1000)
+
+    ## following line takes forever or doesn't halt... I don't know why
+    # accuracy_score = classifier.evaluate(input_fn=get_test_inputs)["accuracy"]
+
+    accuracy = accuracy_score(Y_test, list(classifier.predict(X_test)))
+    print('Accuracy: {0:f}'.format(accuracy))
+
+    # make the prediction
+    print("Making predictions...")
+    predictions = list(classifier.predict(X_predict))
+
+    results_df = pd.DataFrame(data={'y':predictions})
+    joined = pd.DataFrame({'Id':t_id}).join(results_df)
+
+    # Save data in csv format for submission
+    print("Writing predictions to predictions.csv")
+    joined.to_csv("predictions.csv", index=False)
+
+if __name__ == '__main__':
+    main()
--- project3/dnn_dropoutlayers.py
+++ project3/dnn_dropoutlayers.py
@@ -7,7 +7,6 @@ import sklearn.model_selection as sk
 from tensorflow.examples.tutorials.mnist import input_data # TODO remove this
 
 def main():
-    mnist = input_data.read_data_sets('MNIST_data', one_hot=True) # TODO remove this
 
     # read input data from hd5 format
     train = pd.read_hdf("train.h5", "train")
@@ -25,7 +24,14 @@ def main():
     X_predict = np.array(X_predict.values).astype('double')
 
     X_train, X_test, Y_train, Y_test = sk.train_test_split(X,Y,test_size=0.05, random_state=42)
+    # TODO One Hot vector representation
+    Y_trainf = np.zeros((len(Y_train), 5))
+    Y_trainf[(range(len(Y_train)),Y_train)]=1
+    Y_testf = np.zeros((len(Y_test), 5))
+    Y_testf[(range(len(Y_test)),Y_test)]=1
 
+    print((Y_trainf[0:10]))
+    print((Y_train[0:10]))
 
     #logging.getLogger().setLevel(logging.INFO)
     #tf.logging.set_verbosity(tf.logging.INFO)
@@ -36,60 +42,63 @@ def main():
     x = tf.placeholder(tf.float32, shape=[None, 100])
     y_ = tf.placeholder(tf.float32, shape=[None,5])
 
+    W_fc1 = weight_variable([100, 256])
+    b_fc1 = bias_variable([256])
+    y_fc1 = tf.matmul(x, W_fc1) + b_fc1
 
-    W_conv1 = weight_variable([5, 5, 1, 64])
-    b_conv1 = bias_variable([64])
-    x_image = tf.reshape(x, [-1,10,10,1])
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
-    W_conv2 = weight_variable([5, 5, 64, 128])
-    b_conv2 = bias_variable([128])
+    W_fc2 = weight_variable([256, 512])
+    b_fc2 = bias_variable([512])
+    y_fc2 = tf.matmul(y_fc1, W_fc2) + b_fc2
 
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
+    W_fc3 = weight_variable([512, 256])
+    b_fc3 = bias_variable([256])
+    y_fc3 = tf.matmul(y_fc2, W_fc3) + b_fc3
 
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
-    b_fc1 = bias_variable([1024])
+    W_fc4 = weight_variable([256, 5])
+    b_fc4 = bias_variable([5])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    
+    y_conv = tf.nn.tanh(tf.matmul(y_fc3, W_fc4) + b_fc4)
 
-    keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-    W_fc2 = weight_variable([1024, 5])
-    b_fc2 = bias_variable([5])
-
-    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 
     sess.run(tf.global_variables_initializer())
 
-    cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+    train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
     correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     sess.run(tf.global_variables_initializer())
-    # range(100) should be range(20000), run this at home
-    for i in range(1000):
-        # TODO THIS CALL SHOULD FRIGGIN WORK OR YOU ALL DIE
-      batch = next_batch(X_train, Y_train, 0, i)
+    # TODO change this to real max entries of train set
+    batchsize = 100
+    for j in range(2000):
+      i = j % 431
+      start = i*batchsize
+      end = min((i+1)*batchsize, Y_trainf.shape[0])
+      batch = (X_train[start:end], Y_trainf[start:end])
       if i%100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
-            x:batch[0], y_: batch[1], keep_prob: 1.0})
+            x:batch[0], y_: batch[1]})
         print("step %d, training accuracy %g"%(i, train_accuracy))
-      train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+      train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
-    print("test accuracy %g"%accuracy.eval(feed_dict={x: X_test, y_: Y_test, keep_prob: 1.0}))
+    print("test accuracy %g"%accuracy.eval(feed_dict={x: X_test, y_: Y_testf}))
 
-def next_batch(x, y, i, j):
-    # TODO manidaag: write this function in a way that it does its magic in (TODO THIS CALL SHOULD FRIGGIN WORK OR YOU ALL DIE)
-    batch = np.empty(shape=(j-i,2))
-    batch[0] = x[i:j]
-    batch[1] = y[i:j]
-    return batch
+    
+    print("Making predictions...")
+    result = tf.argmax(y_conv.eval(feed_dict={x:X_test}),1)
+    predictions = result.eval()
 
+    results_df = pd.DataFrame(data={'y':predictions})
+    joined = pd.DataFrame({'Id':t_id}).join(results_df)
+
+    # Save data in csv format for submission
+    print("Writing predictions to predictions.csv")
+    joined.to_csv("predictions.csv", index=False)
+
+    
+
+    
 
 
 def weight_variable(shape):
