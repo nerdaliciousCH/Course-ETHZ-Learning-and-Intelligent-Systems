ad568271e35933c2fbe13f36d2c6d79deeb135d5
Fabian Schneider <fabischn@student.ethz.ch>
2017-05-09 12:09:00 +0200

    TODO for manidaag
    Real python pros can do this easily: two nparrays -> into one 2D tensorflow dataset. Do it or die!

--- project3/dnn_dropoutlayers.py
+++ project3/dnn_dropoutlayers.py
@@ -9,23 +9,41 @@ from tensorflow.examples.tutorials.mnist import input_data # TODO remove this
 def main():
     mnist = input_data.read_data_sets('MNIST_data', one_hot=True) # TODO remove this
 
+    # read input data from hd5 format
+    train = pd.read_hdf("train.h5", "train")
+    predict = pd.read_hdf("test.h5", "test")
+
+    # create matrices from input
+    Y = train.iloc[0:,0]
+    X = train.iloc[0:,1:]
+    X_predict = predict.iloc[0:,0:]
+    t_id = predict.index.values
+
+    # type conversion
+    Y = np.array(Y.values).astype('int')
+    X = np.array(X.values).astype('double')
+    X_predict = np.array(X_predict.values).astype('double')
+
+    X_train, X_test, Y_train, Y_test = sk.train_test_split(X,Y,test_size=0.05, random_state=42)
+
+
     #logging.getLogger().setLevel(logging.INFO)
     #tf.logging.set_verbosity(tf.logging.INFO)
     tf.logging.set_verbosity(tf.logging.ERROR)
 
     sess = tf.InteractiveSession()
 
-    x = tf.placeholder(tf.float32, shape=[None, 784])
-    y_ = tf.placeholder(tf.float32, shape=[None,10])
+    x = tf.placeholder(tf.float32, shape=[None, 100])
+    y_ = tf.placeholder(tf.float32, shape=[None,5])
 
 
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
-    x_image = tf.reshape(x, [-1,28,28,1])
+    W_conv1 = weight_variable([5, 5, 1, 64])
+    b_conv1 = bias_variable([64])
+    x_image = tf.reshape(x, [-1,10,10,1])
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
+    W_conv2 = weight_variable([5, 5, 64, 128])
+    b_conv2 = bias_variable([128])
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
@@ -39,8 +57,8 @@ def main():
     keep_prob = tf.placeholder(tf.float32)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-    W_fc2 = weight_variable([1024, 10])
-    b_fc2 = bias_variable([10])
+    W_fc2 = weight_variable([1024, 5])
+    b_fc2 = bias_variable([5])
 
     y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
@@ -54,15 +72,25 @@ def main():
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     sess.run(tf.global_variables_initializer())
     # range(100) should be range(20000), run this at home
-    for i in range(100):
-      batch = mnist.train.next_batch(50)
+    for i in range(1000):
+        # TODO THIS CALL SHOULD FRIGGIN WORK OR YOU ALL DIE
+      batch = next_batch(X_train, Y_train, 0, i)
       if i%100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
             x:batch[0], y_: batch[1], keep_prob: 1.0})
         print("step %d, training accuracy %g"%(i, train_accuracy))
       train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-    print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    print("test accuracy %g"%accuracy.eval(feed_dict={x: X_test, y_: Y_test, keep_prob: 1.0}))
+
+def next_batch(x, y, i, j):
+    # TODO manidaag: write this function in a way that it does its magic in (TODO THIS CALL SHOULD FRIGGIN WORK OR YOU ALL DIE)
+    batch = np.empty(shape=(j-i,2))
+    batch[0] = x[i:j]
+    batch[1] = y[i:j]
+    return batch
+
+
 
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
